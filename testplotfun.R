d267429486d24834b769bc07ea60cb1b69f5c3c2
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-11-08 15:36:14 -0300

    Add files via upload

---
+++ testplotfun.R
@@ -0,0 +1,18 @@
+library (vegan)
+vegdist
+
+
+plot_pi=function(x, plot=FALSE, Method= "co2" ) 
+{
+  if (anyNA (x)==TRUE){stop ("NA values") }
+  Met  <- c("")
+  M.x <- as.matrix(x)
+  if 
+  
+  mutate(H=hour(pi_d3$Hora),
+         D=day(pi_d3$Data),
+         M= month(pi_d3$Data),
+         Y= year(pi_d3$Data))  
+    
+}
+ ?pmatch()?pmatch
